c11cbf23e10372f2f341849fdebe84a073ae504f
cybersam07 <110772605+cybersam07@users.noreply.github.com>
2025-05-10 23:03:01 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -170,8 +170,7 @@ function DashboardCards({ triggerError }: { triggerError: () => void }) {
         <div className="bg-white p-6 rounded-xl shadow-xl border border-gray-100 flex flex-col w-full">
   <div className="font-semibold text-lg mb-4">Last Known Locations</div>
   <div className="aspect-video w-full max-w-2xl mx-auto overflow-hidden rounded-lg border shadow">
-    <img src="/images/tiwa.gif"
-      alt="Map"
+    <img src="./images/tiwa.gif" alt=""
       className="w-full h-full object-cover transition hover:scale-110"
     />
   </div>
